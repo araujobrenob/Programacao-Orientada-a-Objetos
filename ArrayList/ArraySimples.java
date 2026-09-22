b45836667bd8b59a8db9c4cf1ec3c9de7abe072c
brenobezerra2014-cyber <brenobezerra2014@gmail.com>
2026-09-22 10:46:08 -0300

    hashmap arraylist

---
+++ ArrayList/ArraySimples.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArraySimples {
+
+    ArrayList<String>nomes  = Arraylist<String>(); 
+
+    nomes.add("breno");
+    nomes.add("allan");
+    nomes.add("jheymesson");
+    nomes.add("douglas");
+    nomes.add("carlitos tevez");
+
+    nomes.forEach(nomes-> System.out.println(nome));
+
+    
+
+    
+}
---
+++ ArrayList/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main (String[] args){
+
+        ArrayList<String> nomes = ArrayList<String>();
+
+        System.out.println(nomes);
+
+        nomes.add("Leandro");
+        nomes.add("Bianca");
+        nomes.add("Geraldo");
+
+        System.out.println(nomes);
+        
+    }
+}
